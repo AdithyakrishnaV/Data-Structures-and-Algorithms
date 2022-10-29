ca10a9f5aaa7d7d0c86856479438bff7e290944f
AdithyakrishnaV <adithyakr007@gmail.com>
2022-10-29 13:57:04 +0530

    Accept an integer, Print its square value

---
+++ Print_square_value.c
@@ -0,0 +1,13 @@
+//Accept an integer, Print its square value
+
+#include <stdio.h>
+
+int main(){
+    int num;
+    int a;
+    scanf("%d", &num);
+    a= num*num;
+    printf("%d\n", a);
+    
+    return 0;
+}
