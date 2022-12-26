05a29a0053efe580bab608c449ab676a2b59aa92
AdithyakrishnaV <adithyakr007@gmail.com>
2022-12-26 14:58:40 +0530

    add search and sort

--- cp/Bubble_sort.c
+++ cp/Bubble_sort.c
@@ -1,16 +1,9 @@
-/*
-The bubble sort gets its name because elements tend to move 
-up into the correct order like bubbles rising to the surface. 
-
-Bigger elements bubble up lower elements under it
-*/
-
 #include <stdio.h>
 
 int main(void) 
 {
     int n;
-    scanf("%d", &n);
+    scanf("%d ", &n);
     //declare array
     int a[n];
     //take input of array
@@ -19,18 +12,19 @@ int main(void)
     }
 
     //run loop for all elements in array
-    for(int j=0; j<n; j++){
+    for(int j=0; j<n-1; j++){//2   n
+
         //run loop throughout the array
-        for(int i=0; i<n; i++){
+
+        for(int i=0; i<n-1-j; i++){
+        
             if(a[i]>a[i+1]){
-                int f = a[i+1];//f=0
-                a[i+1] = a[i];//i+1=45
-                a[i] = f;//a[i]=0
+                int f = a[i+1];
+                a[i+1] = a[i];
+                a[i] = f;
             }
         }
     }
-    // Write the rest of your code here
-    //Use int a[n] as aay of size n
     
     for(int i=0;i<n;i++){
     printf("%d ",a[i]);
