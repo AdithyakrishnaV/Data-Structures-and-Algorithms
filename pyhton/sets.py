329d70b00d05de114432e4561e658a2ed664d369
AdithyakrishnaV <adithyakr007@gmail.com>
2023-02-23 18:42:23 +0530

    sets different features

--- pyhton/sets.py
+++ pyhton/sets.py
@@ -1,60 +1,60 @@
-# #Sets - unordered, mutable, no duplicates
+#Sets - unordered, mutable, no duplicates
 
-# # myset={1,2,3,4}
-# # myset=set([1,2,3,4])
+# myset={1,2,3,4}
+# myset=set([1,2,3,4])
 
-# #create an empty set
-# s=set()
+#create an empty set
+s=set()
 
-# s.add(1)
-# s.add(3)
-# s.add(4)
+s.add(1)
+s.add(3)
+s.add(4)
 
-# # s.discard(3)
-# print(s.pop())
-# # s.clear() #clear all elements from the set
-# # s.remove(5) 
-# # # KeyError: 5  when given a number not in the set
-# print(s)
+# s.discard(3)
+print(s.pop())
+# s.clear() #clear all elements from the set
+# s.remove(5) 
+# # KeyError: 5  when given a number not in the set
+print(s)
 
-# odd=set([0,1,3,5])
-# even=set([2,4,6])
-# even=set([1,2,3,4,5])
-# #find union in built  feature
-# u=odd.union(even)
-# #find intersection in built  feature
-# a=odd.intersection(even)
+odd=set([0,1,3,5])
+even=set([2,4,6])
+even=set([1,2,3,4,5])
+#find union in built  feature
+u=odd.union(even)
+#find intersection in built  feature
+a=odd.intersection(even)
 
-# print(u)
-# print(a)
-# setA={1,2,3,4,5,6,7,8,9,10}
-# setB={2,4,6,8,9}
-# diff=setA.difference(setB)
-# #{1, 3, 5, 7, 10}
-# print(diff)
-# dif=setB.difference(setA)
-# #set()
+print(u)
+print(a)
+setA={1,2,3,4,5,6,7,8,9,10}
+setB={2,4,6,8,9}
+diff=setA.difference(setB)
+#{1, 3, 5, 7, 10}
+print(diff)
+dif=setB.difference(setA)
+#set()
 
-# setA={1,2,3,4,5,6,7,8,9,10}
-# setB={2,4,6,8,9}
-# m=setB.issubset(setA)
-# print(m)
-# #true
+setA={1,2,3,4,5,6,7,8,9,10}
+setB={2,4,6,8,9}
+m=setB.issubset(setA)
+print(m)
+#true
 
-# setA={1,2,3,4,5,6,7,8,9,10}
-# setB={2,4,6,8,9}
-# m=setB.issuperset(setA)
-# print(m)
-# #False
+setA={1,2,3,4,5,6,7,8,9,10}
+setB={2,4,6,8,9}
+m=setB.issuperset(setA)
+print(m)
+#False
 
-# setA={1,2,3,4,5,6,7,8,9,10}
-# #setB=setA.copy()
-# #or
-# setB=set(setA)
+setA={1,2,3,4,5,6,7,8,9,10}
+#setB=setA.copy()
+#or
+setB=set(setA)
 
-# setB.add(100)
-# print(setA)
-# print(setB)
-# #result:
-#     # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-#     # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100}
+setB.add(100)
+print(setA)
+print(setB)
+#result:
+    # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100}
