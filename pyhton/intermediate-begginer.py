9e1a1fd1910cb13670e2a54b5a0dcea6dbbd3f44
AdithyakrishnaV <adithyakr007@gmail.com>
2023-02-25 17:15:07 +0530

    features of LISTS

--- pyhton/intermediate-begginer.py
+++ pyhton/intermediate-begginer.py
@@ -1,35 +1,3 @@
-# #________________________LIST_____:- ordered, mutable, allows duplicate elements
-list=["adi",1,True]
-
-list.append("cybercookie")
-list.pop()
-list.reverse()
-# print(list)
-nolist=[1,2,3,4,5,6,7,8]
-# print(nolist[3:])
-# print(nolist[1:6])
-# print(nolist[:3])
-b=nolist[::3] #every third index 
-c=nolist[::-1]#reverse
-print(b)
-
-#use .copy() to copy a list other wise it will tamper the orginal list
-li=nolist #both will refer to the same location
-li.append("adi")
-print(li)
-print(nolist)
-#result: 
-# [1, 2, 3, 4, 5, 6, 7, 8, 'adi']
-# [1, 2, 3, 4, 5, 6, 7, 8, 'adi']
-li=nolist.copy()
-#or 
-li=nolist[:]
-li.append("adi")
-print(li)
-print(nolist)
-#result:
-# [1, 2, 3, 4, 5, 6, 7, 8, 'adi']
-# [1, 2, 3, 4, 5, 6, 7, 8]
 
 
 #--------------------------------------
